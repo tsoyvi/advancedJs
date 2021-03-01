2644491ba8e03f60fcb133ce081162ff3eb2f476
tsoyvi <tsoyvi@inbox.ru>
2021-03-01 23:32:33 +0500

    Урок 3 (загрузка корзины с сервера)

--- lesson3/project/js/main.js
+++ lesson3/project/js/main.js
@@ -43,6 +43,9 @@ class ProductList {
 
     this.#getProducts()
       .then((data) => {
+
+        console.log (data);
+
         this.#goods = [...data];
         this.#render();
       });
@@ -66,7 +69,7 @@ class ProductList {
 
     this.#goods.forEach((product) => {
       const productObject = new ProductItem(product);
-     // console.log(productObject);
+      // console.log(productObject);
       this.#allProducts.push(productObject);
       block.insertAdjacentHTML('beforeend', productObject.render());
     });
@@ -93,4 +96,101 @@ class ProductItem {
   }
 }
 
+
+
+/****************************************************** */
+
+class ProductCart {
+  #goods;
+  #allProducts;
+
+  constructor(container = ".cart-block", url = "/getBasket.json") {
+    this.container = container;
+    this.#goods = [];
+    this.#allProducts = [];
+    this.amount = 0; // !!!!!!!!!
+    this.countGoods = 0; // !!!!!!!!!!!
+
+    this.#getProducts(url)
+      .then((data) => {
+        
+        console.log (data);
+        this.amount = data.amount;
+        this.countGoods = data.countGoods;
+
+        this.#goods = [...data.contents];
+
+        this.#render();
+      });
+  }
+
+
+  #getProducts(url) {
+    return fetch(API + "" + url)
+      .then((response) => response.json())
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+
+  #render() {
+    const block = document.querySelector(this.container);
+    this.#goods.forEach((product) => {
+      const productObject = new CartItem(product);
+      this.#allProducts.push(productObject);
+      block.insertAdjacentHTML('beforeend', productObject.render());
+    });
+  }
+
+
+
+}
+
+
+// Никак не получается наследование
+// Нужно сначала тренироваться на простых задачках
+
+
+class CartItem {
+
+  constructor(product, img = 'https://placehold.it/50x37') {
+    
+    this.productName = product.product_name;
+    this.price = product.price;
+    this.id = product.id_product;
+    this.img = img;
+    this.quantity = product.quantity;
+
+  }
+
+  render() {
+    return `<div class="cart-item" data-id="${this.id}">
+      <div class="product-bio">
+        <img src="${this.img}" alt="Some image">
+          <div class="product-desc">
+            <p class="product-title">${this.productName}</p>
+            <p class="product-quantity">Количество: ${this.quantity}</p>
+            <p class="product-single-price">${this.price} за ед.</p>
+          </div>
+    </div>
+        <div class="right-block">
+          <p class="product-price">${this.quantity * this.price} ₽</p>
+          <button class="del-btn" data-id="${this.id}">&times;</button>
+        </div>
+      </div>`;
+  }
+
+}
+
+
+
+
+
+
+
+/********************************************************** */
 const productList = new ProductList();
+
+
+const productCart = new ProductCart();
