aa56d46f875b2f477a26973f0c276ec2541101be
tsoyvi <tsoyvi@inbox.ru>
2021-03-26 00:00:06 +0500

    Урок 8 начало проекта

---
+++ src/components/ProductItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="items-arr">
+    <div class="items shadow">
+     <img src="@/assets/img/Image_placeholder_1.png" alt="Image_placeholder_1.png">
+        <p class="items-name">{{product.product_name}}</p>
+        <p class="items-price">${{product.price}}</p>
+        <div class="items-overlay">
+           <button class="items-button-cart">
+           <img src="@/assets/img/cart-white.png" alt="cart-white.png"
+           class="items-button-cart-img">
+           Add to cart
+           </button>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductItem',
+  props: ['product'],
+};
+</script>
---
+++ src/components/ProductsList.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <h1>productsList</h1>
+    <product-item v-for="(product, index) of products" :product="product" :key="index">
+    </product-item>
+  </div>
+</template>
+
+<script>
+
+import ProductItem from './ProductItem.vue';
+
+export default {
+  components: { ProductItem },
+  name: 'ProductsList',
+  data() {
+    return {
+      products: [
+        {
+          id_product: 124,
+          product_name: 'Mango People T-shirt',
+          price: 100.00,
+          img: 'Image_placeholder_1.png',
+
+        },
+        {
+          id_product: 457,
+          product_name: 'Mango People T-shirt',
+          price: 325.60,
+          img: 'Image_placeholder_2.png',
+        },
+        {
+          id_product: 123,
+          product_name: 'Mango People T-shirt',
+          price: 22.60,
+          img: 'Image_placeholder_3.png',
+        },
+        {
+          id_product: 413,
+          product_name: 'Mango People T-shirt',
+          price: 34.32,
+          img: 'Image_placeholder_4.png',
+        },
+        {
+          id_product: 456,
+          product_name: 'Mango People T-shirt',
+          price: 34.32,
+          img: 'Image_placeholder_5.png',
+        },
+        {
+          id_product: 413,
+          product_name: 'Mango People T-shirt',
+          price: 24.90,
+          img: 'Image_placeholder_6.png',
+        },
+        {
+          id_product: 331,
+          product_name: 'Mango People T-shirt',
+          price: 53.03,
+          img: 'Image_placeholder_7.png',
+        },
+        {
+          id_product: 952,
+          product_name: 'Mango People T-shirt',
+          price: 5.99,
+          img: 'Image_placeholder_8.png',
+        },
+        {
+          id_product: 3414,
+          product_name: 'Mango People T-shirt',
+          price: 52.92,
+          img: 'Image_placeholder_9.png',
+        },
+
+      ],
+
+    };
+  },
+
+};
+</script>
+
+<style scoped lang="scss">
+</style>
